c8fe087ae73fe8eb4b6798a806081949fedf2710
serafm <serafm@protonmail.com>
2024-08-06 00:02:42 +0300

    Remove CMakeList classes

---
+++ cmake-build-debug/CMakeFiles/rtree_cpp_v2.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtree_cpp_v2.dir/src/Collections/FloatVector.cpp.o"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Collections/FloatVector.cpp.o.d"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Collections/IntVector.cpp.o"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Collections/IntVector.cpp.o.d"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Collections/PriorityQueue.cpp.o"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Collections/PriorityQueue.cpp.o.d"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Main.cpp.o"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Main.cpp.o.d"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Node.cpp.o"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Node.cpp.o.d"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Point.cpp.o"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Point.cpp.o.d"
+  "CMakeFiles/rtree_cpp_v2.dir/src/RTree.cpp.o"
+  "CMakeFiles/rtree_cpp_v2.dir/src/RTree.cpp.o.d"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Rectangle.cpp.o"
+  "CMakeFiles/rtree_cpp_v2.dir/src/Rectangle.cpp.o.d"
+  "rtree_cpp_v2"
+  "rtree_cpp_v2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtree_cpp_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/rtree_cpp_v2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rtree_cpp_v2.
+# This may be replaced when dependencies are built.
